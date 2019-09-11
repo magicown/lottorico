4c568f1d2f7fb5b2ad79ae34822c5c52efd756bb
Annica2011 <phpprogram47@gmail.com>
2019-09-11 22:53:00 +0900

    Update dbconfig.php
    Db connect id,pw delete

--- www/data/dbconfig.php
+++ www/data/dbconfig.php
@@ -1,9 +1,9 @@
 <?php
 if (!defined('_GNUBOARD_')) exit;
 define('G5_MYSQL_HOST', 'localhost');
-define('G5_MYSQL_USER', 'lotto');
-define('G5_MYSQL_PASSWORD', 'pTjVTPO1G8Ewizw9');
-define('G5_MYSQL_DB', 'lotto');
+define('G5_MYSQL_USER', '');
+define('G5_MYSQL_PASSWORD', '');
+define('G5_MYSQL_DB', '');
 define('G5_MYSQL_SET_MODE', false);
 
 define('G5_TABLE_PREFIX', 'g5_');
